11bde524f8fe3ba3e34881b3228f0ceccfd23883
Hv0nnus <tanguy.kerdoncuff@laposte.net>
2018-03-27 13:29:25 +0200

    add python file test and the bash file to execute python file

---
+++ test_cluster.py
@@ -0,0 +1,14 @@
+
+a = 4
+
+print(a)
+b = a+1
+print(b)
+
+header_confMat = ["Set","Value","Target","Prediction","Epoch"]
+header_loss = ["Value","Set","Epoch"]
+    
+# Try to open the file and write the header
+#with open("CSV_confMat.csv", 'w') as csvfile:
+#        cwriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+#        cwriter.writerow(header_confMat)
