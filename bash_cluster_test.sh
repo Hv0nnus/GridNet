6e29e8d274a9ec3fb27294143478ed789b7ffebf
Hv0nnus <tanguy.kerdoncuff@laposte.net>
2018-03-27 17:05:23 +0200

    few modification in Main

--- bash_cluster_test.sh
+++ bash_cluster_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J test_slurm_gpu
+#SBATCH -J test_slurm
 #SBATCH -o Output/output_%j.txt
 #SBATCH -D /home_expes/kt82128h/GridNet/
 #SBATCH -n 1
@@ -10,11 +10,9 @@
 #SBATCH --mem=3G
 #SBATCH -p SHORT
 #SBATCH -x calcul-gpu-lahc-2
-#    SBATCH -D /home_expes/gt78520h
-#
 
-source /home_expes/tools/python/python3_gpu
+source /home_expes/tools/python/python3_cpu
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export LD_LIBRARY_PATH=/home_expes/tools/cuda/cuda-8.0/lib64:$LD_LIBRARY_PATH
 
-srun -N1 -n1 -c$SLURM_CPUS_PER_TASK  python test_cluster.py  
+srun -N1 -n1 -c$SLURM_CPUS_PER_TASK  python Main.ipynb  
