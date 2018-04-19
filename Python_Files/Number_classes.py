21d65df16926e9807ad685b91a7512813621286c
Hv0nnus <tanguy.kerdoncuff@laposte.net>
2018-04-19 10:29:30 +0200

    print something during the number classes function

--- Python_Files/Number_classes.py
+++ Python_Files/Number_classes.py
@@ -74,6 +74,9 @@ def f():
 
     for i, (x_batch, y_batch, _) in enumerate(train_loader):
         count += count_classes(y_batch, parameters)
+            with open("/home_expes/kt82128h/GridNet/Python_Files/Python_print.txt", 'a') as txtfile:
+                txtfile.write("\n " "Loop : " + str(i) + "count : " +str(count) + "\n")
+
     count = count / sum(count)
     print(count)
     return (1 / count)
